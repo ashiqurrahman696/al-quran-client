4ef9fa81378162cb8f41d449ff1e25b9131857dd
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-05-15 09:59:11 +0600

    updated url

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
   
   // todo: Load reciters
   useEffect(() => {
-    axios.get('http://localhost:5173/reciters.json')
+    axios.get('http://al-quran-2.netlify.app/reciters.json')
     .then(res => setReciters(res.data))
     .catch(err => console.log(err));
   }, [axios]);
